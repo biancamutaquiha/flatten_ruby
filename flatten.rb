f11e534c01ae1baa76c194f5a6fab88541c0cf72
biancamutaquiha <biafersil@gmail.com>
2022-05-09 09:50:03 +0200

    add test case not_array_input?

--- flatten.rb
+++ flatten.rb
@@ -18,8 +18,13 @@ def empty_array?
 end
 
 def flatten_array?
-    assert_equal([1,2,3], flatten([1,2,3]))
+  assert_equal([1,2,3], flatten([1,2,3]))
+end
+
+def not_array_input?
+  assert_equal('input must be array', flatten(1))
 end
 
 empty_array?
 flatten_array?
+not_array_input?
