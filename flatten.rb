076612a611e09dbcd6d1f163cbb39668cf5aabc8
biancamutaquiha <biafersil@gmail.com>
2022-05-09 09:34:51 +0200

    add empty_array implementation

--- flatten.rb
+++ flatten.rb
@@ -1,12 +1,17 @@
+def flatten(array)
+  new_array = []
+  new_array
+end
+
 def assert_equal(expected, actual)
-    if expected != actual
-      puts "Expected: #{expected.inspect}, got: #{actual.inspect}"
-      exit 1
-    end
+  if expected != actual
+    puts "Expected: #{expected.inspect}, got: #{actual.inspect}"
+    exit 1
+  end
 end
 
 def empty_array?
-    assert_equal([], flatten([]))
+  assert_equal([], flatten([]))
 end
 
 empty_array?
