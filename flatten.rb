c66114ef10387c3ea707c1100e0c2d12b7515c35
biancamutaquiha <biafersil@gmail.com>
2022-05-09 09:24:22 +0200

    add test case empty_array?

---
+++ flatten.rb
@@ -0,0 +1,12 @@
+def assert_equal(expected, actual)
+    if expected != actual
+      puts "Expected: #{expected.inspect}, got: #{actual.inspect}"
+      exit 1
+    end
+end
+
+def empty_array?
+    assert_equal([], flatten([]))
+end
+
+empty_array?
